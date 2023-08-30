643fc5d08ac3f129b250bee6c65bc3d1f9e4c1bc
DANIEL ZAN <99081463+Daniel-Zan@users.noreply.github.com>
2023-08-30 18:54:37 -0500

    ah

---
+++ cursos_Clases/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cursos_Clases',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
