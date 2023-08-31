8027c7aa9b92139a715059872a4cc7bf8346867b
DANIEL ZAN <99081463+Daniel-Zan@users.noreply.github.com>
2023-08-30 19:57:07 -0500

    Boton para ir a estudiantes

--- cursos_Clases/src/app/courses/courses.page.ts
+++ cursos_Clases/src/app/courses/courses.page.ts
@@ -2,7 +2,7 @@ import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { IonInput } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 interface Curso {
   id: number;
